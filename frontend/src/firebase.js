3971f9094e93084cf38d251aa0f361da1229aabb
shubhamjatania17 <shubham.jatania@gmail.com>
2026-01-17 18:26:23 +0530

    Firebase Login added

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,9 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_WEB_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
+  apiKey: "AIzaSyCzav8w5ExiY9Xi3Y0PIfDPi9xjVQjtpXE",
+  authDomain: "gen-lang-client-0308678139.firebaseapp.com",
+  projectId: "gen-lang-client-0308678139",
+  storageBucket: "gen-lang-client-0308678139.firebasestorage.app",
+  messagingSenderId: "760622276558",
+  appId: "1:760622276558:web:4bdb795387163734ec559f",
+  measurementId: "G-T2XFBG1SP2"
 };
 
 const app = initializeApp(firebaseConfig);
