ba4f33d042b0d97d2ddfe2b533f76322582b7ef6
shubhamjatania17 <shubham.jatania@gmail.com>
2026-01-17 20:41:03 +0530

    console log message

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,11 @@ import "dotenv/config";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 8080;
+app.use((req, res, next) => {
+  console.log("REQ:", req.method, req.url);
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
